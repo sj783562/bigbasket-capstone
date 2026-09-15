7a0f2561ca23017e4cb6f3f177cc517f2cf469be
sj783562 <sarthakjoshi538@gmail.com>
2026-09-15 11:27:20 +0530

    Add verify_data.sql

--- verify_data.sql
+++ verify_data.sql
@@ -1,23 +1,23 @@
 -- Expected: 31 products
-SELECT COUNT(*) 
+SELECT COUNT(*) AS total_products
 FROM products;
 
 -- Expected: 50 customers
-SELECT COUNT(*) 
+SELECT COUNT(*) AS total_customers
 FROM customers;
 
 -- Expected: 500 orders
-SELECT COUNT(*) 
+SELECT COUNT(*) AS total_orders
 FROM orders;
 
 -- Expected: 6 category targets
-SELECT COUNT(*) 
+SELECT COUNT(*) AS total_category
 FROM category_targets;
 
 -- Expected: Delivered 434, Cancelled 42, Pending 24
 SELECT 
   status, 
-  COUNT(*) 
+  COUNT(*) AS total_no_of_each_delivery
   
 FROM orders 
 GROUP BY status;
